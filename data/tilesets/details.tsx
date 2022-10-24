8b5478fb96701f4b42dc96a6e8694d9856935267
Gieneq <piotr.adamczyk.gda@gmail.com>
2022-10-24 15:38:44 +0200

    Graphics update

--- data/tilesets/details.tsx
+++ data/tilesets/details.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="details" tilewidth="20" tileheight="20" tilecount="225" columns="15">
- <image source="../Graphics/details.png" width="300" height="300"/>
+ <image source="details.png" width="300" height="300"/>
 </tileset>
--- data/tilesets/floor.tsx
+++ data/tilesets/floor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="floor" tilewidth="20" tileheight="20" tilecount="225" columns="15">
- <image source="../Graphics/floor.png" width="300" height="300"/>
+ <image source="floor.png" width="300" height="300"/>
 </tileset>
